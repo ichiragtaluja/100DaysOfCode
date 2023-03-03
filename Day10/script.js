fd574fa68b0221064a82154f7529594265d84a46
ichiragtaluja <Ichiragtaluja@gmail.com>
2023-03-04 03:06:51 +0530

    add practice questions

--- Day10/script.js
+++ Day10/script.js
@@ -1,23 +1,32 @@
-
-1// Given an array of objects representing people, write an ES6 function to return a new array containing only the names of the people.
-
-
-const people = [
-    { name: 'Raj', age: 28 },
-    { name: 'Swapnil', age: 42 },
-    { name: 'Anushka', age: 35 }
-  ];
-  
-  const itsAFunctionThatGoesInsideMapOrFilter = ({name}) => name
-  const itsAFunction = arrayOfObjects => arrayOfObjects.map(itsAFunctionThatGoesInsideMapOrFilter)
-  
-  console.log(itsAFunction(people))
+1 // Given an array of objects representing people, write an ES6 function to return a new array containing only the names of the people.
 
 
+const people = [{
+        name: 'Raj',
+        age: 28
+    },
+    {
+        name: 'Swapnil',
+        age: 42
+    },
+    {
+        name: 'Anushka',
+        age: 35
+    }
+];
+
+const itsAFunctionThatGoesInsideMapOrFilter = ({
+    name
+}) => name
+const itsAFunction = arrayOfObjects => arrayOfObjects.map(itsAFunctionThatGoesInsideMapOrFilter)
+
+console.log(itsAFunction(people))
 
 
 
-2//   Given an array, write an ES6 function to return a new array with all the elements multiplied by 5.
+
+
+2 //   Given an array, write an ES6 function to return a new array with all the elements multiplied by 5.
 
 const numbers = [1, 2, 3, 4];
 // Your code here
@@ -35,16 +44,27 @@ console.log(multiplyByFive); // Output: [5, 10, 15, 20]
 
 
 
-3// Given employee’s data, write an ES6 function which greets them with a personalized message for onboarding.
+3 // Given employee’s data, write an ES6 function which greets them with a personalized message for onboarding.
 
-const employeeData = [
-	{name: "ram", dept: "marketer"}, 
-	{name: "Radha", dept: "SDE"},
-	{name: "shyam", dept: "finance professional"},
+const employeeData = [{
+        name: "ram",
+        dept: "marketer"
+    },
+    {
+        name: "Radha",
+        dept: "SDE"
+    },
+    {
+        name: "shyam",
+        dept: "finance professional"
+    },
 ]
 
 // Your code here
-const greetEmployeeMessages = array => array.map(({name,dept}) => `Hi ${name} we are glad to have you as a ${dept}`)
+const greetEmployeeMessages = array => array.map(({
+    name,
+    dept
+}) => `Hi ${name} we are glad to have you as a ${dept}`)
 
 
 
@@ -59,20 +79,31 @@ console.log(greetEmployeeMessages);
 
 
 // 4. Write an ES6 function that takes an array of objects representing books and returns an array with only the titles of each book.
-    
-  
-const books = [
-    { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
-    { title: 'To Kill a Mockingbird', author: 'Harper Lee' },
-    { title: '1984', author: 'George Orwell' },
-    { title: 'Pride and Prejudice', author: 'Jane Austen' },
-  ];
-  
-  // Your code here
-  const getBookTitles = books => books.map(book => book.title)
-  
-  const titles = getBookTitles(books);
-  console.log(titles); // Output: ['The Great Gatsby', 'To Kill a Mockingbird', '1984', 'Pride and Prejudice']
+
+
+const books = [{
+        title: 'The Great Gatsby',
+        author: 'F. Scott Fitzgerald'
+    },
+    {
+        title: 'To Kill a Mockingbird',
+        author: 'Harper Lee'
+    },
+    {
+        title: '1984',
+        author: 'George Orwell'
+    },
+    {
+        title: 'Pride and Prejudice',
+        author: 'Jane Austen'
+    },
+];
+
+// Your code here
+const getBookTitles = books => books.map(book => book.title)
+
+const titles = getBookTitles(books);
+console.log(titles); // Output: ['The Great Gatsby', 'To Kill a Mockingbird', '1984', 'Pride and Prejudice']
 
 
 
@@ -81,7 +112,7 @@ const books = [
 
 
 
-5//   Write an ES6 function which takes out the names of the students whose first letter starts with ‘A’.
+5 //   Write an ES6 function which takes out the names of the students whose first letter starts with ‘A’.
 
 const studentName = ["Ram", "Anjali", "Arpit", "Bhanu Kumar", "Jaya", "Ankit", "shayam"]
 // Your code here
@@ -97,13 +128,24 @@ console.log(studentNames);
 
 
 
-6// Write an ES6 function which filters out the products which have a price greater than 40.
+6 // Write an ES6 function which filters out the products which have a price greater than 40.
 
-const productData = [
-    {prodName: "Dairy Milk", price: 10},
-    {prodName: "Dairy Milk Silk", price: 70},
-    {prodName: "Five Star", price: 20},
-    {prodName: "Mars", price: 50}
+const productData = [{
+        prodName: "Dairy Milk",
+        price: 10
+    },
+    {
+        prodName: "Dairy Milk Silk",
+        price: 70
+    },
+    {
+        prodName: "Five Star",
+        price: 20
+    },
+    {
+        prodName: "Mars",
+        price: 50
+    }
 ]
 // Your code here
 const getProducts = (array, num) => array.filter(pro => pro.price > num)
@@ -120,12 +162,12 @@ console.log(getProducts(productData, 40))
 
 
 
-7// Write an ES6 function that takes an array of numbers and returns the first number that is divisible by 7.
+7 // Write an ES6 function that takes an array of numbers and returns the first number that is divisible by 7.
 
 const numbers = [1, 2, 3, 21, 14, 7];
 // Your code here
 
-const isDivisibleBy7 = array => array.find(num => num%7 ===0)
+const isDivisibleBy7 = array => array.find(num => num % 7 === 0)
 
 
 console.log(isDivisibleBy7(numbers))
@@ -136,9 +178,9 @@ console.log(isDivisibleBy7(numbers))
 
 
 
-8// Write an ES6 function that takes an array of strings and returns the first string that is longer than 8 characters.
+8 // Write an ES6 function that takes an array of strings and returns the first string that is longer than 8 characters.
 
-const names = ["Mohan", "Anjali", "Geetanjali", "Ankit", "Bhanu Kumar", "Ramakrishnan",  "shayam"]
+const names = ["Mohan", "Anjali", "Geetanjali", "Ankit", "Bhanu Kumar", "Ramakrishnan", "shayam"]
 // Your code here
 const isNamesGreaterThan8 = names => names.filter(name => name.length > 8)
 
@@ -150,23 +192,34 @@ console.log(isNamesGreaterThan8(names));
 
 
 
-9// . Write an ES6 function that takes an array of objects representing students with properties name and grade. Return the first student object that has a grade of "A".
+9 // . Write an ES6 function that takes an array of objects representing students with properties name and grade. Return the first student object that has a grade of "A".
 
-const students = [
-    { name: "John", grade: "B" },
-    { name: "Mary", grade: "A" },
-    { name: "Sam", grade: "C" },
-    { name: "Sarah", grade: "A" },
-  ];
-  
-  // Your code here
-  const findStudentWithGradeA = students => students.find(student => student.grade === "A") 
-  
-  
-  
-  const studentWithGradeA = findStudentWithGradeA(students);
-  console.log(studentWithGradeA); 
-  // Output: { name: "Mary", grade: "A" }
+const students = [{
+        name: "John",
+        grade: "B"
+    },
+    {
+        name: "Mary",
+        grade: "A"
+    },
+    {
+        name: "Sam",
+        grade: "C"
+    },
+    {
+        name: "Sarah",
+        grade: "A"
+    },
+];
+
+// Your code here
+const findStudentWithGradeA = students => students.find(student => student.grade === "A")
+
+
+
+const studentWithGradeA = findStudentWithGradeA(students);
+console.log(studentWithGradeA);
+// Output: { name: "Mary", grade: "A" }
 
 
 
@@ -174,47 +227,98 @@ const students = [
 
 
 //   10 Write an ES6 function that takes an array of objects representing students with properties name, grade and scholarship. Return the first student object that has a grade of "A" or they are a scholarship student.
-  
-
-const students = [
-    { name: "John", grade: "B", scholarship: false },
-    { name: "Mary", grade: "B", scholarship: true },
-    { name: "Sam", grade: "A", scholarship: false },
-    { name: "Sarah", grade: "A", scholarship: true },
-  ];
-  
-  // Your code here
-  const  findStudent = student => student.find(person => person.grade === "A" || person.scholarship) 
-  
-  
-  
-  const student = findStudent(students);
-  console.log(student); 
-  // Output: { name: "Mary", grade: "B", scholarship: true }
 
 
+const students = [{
+        name: "John",
+        grade: "B",
+        scholarship: false
+    },
+    {
+        name: "Mary",
+        grade: "B",
+        scholarship: true
+    },
+    {
+        name: "Sam",
+        grade: "A",
+        scholarship: false
+    },
+    {
+        name: "Sarah",
+        grade: "A",
+        scholarship: true
+    },
+];
 
-
-12//   Write an ES6 function that takes an array of objects containing Bollywood movie information (title, director, year, rating) and returns an array with only the movies that were made before 1990 and has a rating above 8.0. (Question Level: tough)
-
-
-const bollywoodMovies = [
-    { title: 'Sholay', director: 'Ramesh Sippy', year: 1975, rating: 8.2 },
-    { title: 'Amar Akbar Anthony', director: 'Manmohan Desai', year: 1977, rating: 7.6 },
-    { title: 'Namak Halaal', director: 'Prakash Mehra', year: 1982, rating: 7.4 },
-    { title: 'Mr. India', director: 'Shekhar Kapur', year: 1987, rating: 7.8 },
-    { title: 'Qayamat Se Qayamat Tak', director: 'Mansoor Khan', year: 1988, rating: 7.6 },
-    { title: 'Parinda', director: 'Vidhu Vinod Chopra', year: 1989, rating: 8.1 },
-    { title: 'Dil', director: 'Indra Kumar', year: 1990, rating: 7.8 }
-  ];
-  
-  // Your code here
-  const getBestOldBollywoodMovies = movies => movies.filter(({year,rating}) => year < 1990 && rating > 8).map(({title})=>title)
-  
-  
-  
-  const bestOldMovies = getBestOldBollywoodMovies(bollywoodMovies);
-  console.log(bestOldMovies); // Output: ['Sholay', 'Parinda']
+// Your code here
+const findStudent = student => student.find(person => person.grade === "A" || person.scholarship)
 
 
 
+const student = findStudent(students);
+console.log(student);
+// Output: { name: "Mary", grade: "B", scholarship: true }
+
+
+
+
+12 //   Write an ES6 function that takes an array of objects containing Bollywood movie information (title, director, year, rating) and returns an array with only the movies that were made before 1990 and has a rating above 8.0. (Question Level: tough)
+
+
+const bollywoodMovies = [{
+        title: 'Sholay',
+        director: 'Ramesh Sippy',
+        year: 1975,
+        rating: 8.2
+    },
+    {
+        title: 'Amar Akbar Anthony',
+        director: 'Manmohan Desai',
+        year: 1977,
+        rating: 7.6
+    },
+    {
+        title: 'Namak Halaal',
+        director: 'Prakash Mehra',
+        year: 1982,
+        rating: 7.4
+    },
+    {
+        title: 'Mr. India',
+        director: 'Shekhar Kapur',
+        year: 1987,
+        rating: 7.8
+    },
+    {
+        title: 'Qayamat Se Qayamat Tak',
+        director: 'Mansoor Khan',
+        year: 1988,
+        rating: 7.6
+    },
+    {
+        title: 'Parinda',
+        director: 'Vidhu Vinod Chopra',
+        year: 1989,
+        rating: 8.1
+    },
+    {
+        title: 'Dil',
+        director: 'Indra Kumar',
+        year: 1990,
+        rating: 7.8
+    }
+];
+
+// Your code here
+const getBestOldBollywoodMovies = movies => movies.filter(({
+    year,
+    rating
+}) => year < 1990 && rating > 8).map(({
+    title
+}) => title)
+
+
+
+const bestOldMovies = getBestOldBollywoodMovies(bollywoodMovies);
+console.log(bestOldMovies); // Output: ['Sholay', 'Parinda']
--- Day10/script2.js
+++ Day10/script2.js
@@ -7,7 +7,12 @@ output = ["apple 5", "ban 3"]
 input = 'you will win'
 output = ["you 3", "will 4", "win 3"]
 
-const anonymousFunction = string => string.split(" ").map(word => ${word} ${word.length})
+const anonymousFunction = string => string.split(" ").map(word => $ {
+        word
+    }
+    $ {
+        word.length
+    })
 
 console.log(anonymousFunction('you will win'))
 
@@ -18,81 +23,91 @@ console.log(anonymousFunction('you will win'))
 // Given an array of objects, write a code for "getOutput" function to return the first object that contains a specific given key-value pair.
 // eg:
 
-const arr = [{name: "John", age: 25}, {name: "Sarah", age: 30}, {name: "Bob", age: 20}]
-const key="age"
-const value=25
+const arr = [{
+    name: "John",
+    age: 25
+}, {
+    name: "Sarah",
+    age: 30
+}, {
+    name: "Bob",
+    age: 20
+}]
+const key = "age"
+const value = 25
 
-console.log(getOutput(arr,key,value)); // Output: {name: "John", age: 25}
+console.log(getOutput(arr, key, value)); // Output: {name: "John", age: 25}
 
-const getOutput = (arr, key, value) => arr.find (object => object[key]  ===  value
-
-
+const getOutput = (arr, key, value) => arr.find(object => object[key] === value
 
 
 
-// Given an array of strings, use the filter method to return a new array containing only the strings that are palindromes.
-Input: ["racecar", "banana", "level", "apple"]
-Output: ["racecar", "level"]
 
 
-const isStringPalindrome = str =>{
-    for ( let i = 0; i < str.length; i++){
-        if(str[i] !== str[str.length - 1 - i]){
-            return false
+        // Given an array of strings, use the filter method to return a new array containing only the strings that are palindromes.
+        Input: ["racecar", "banana", "level", "apple"] Output: ["racecar", "level"]
+
+
+        const isStringPalindrome = str => {
+            for (let i = 0; i < str.length; i++) {
+                if (str[i] !== str[str.length - 1 - i]) {
+                    return false
+                }
+            }
+            return true
         }
-    } return true
-}
 
-const anonymouFunction = array => array.filter(isStringPalindrome)
+        const anonymouFunction = array => array.filter(isStringPalindrome)
 
 
 
-// Write a function to calculate sum of n elements where n >=2
+        // Write a function to calculate sum of n elements where n >=2
 
-console.log(sumOfN(1,2,3)) // output: 6
-console.log(sumOfN(1,2,3,4)) // output: 10
+        console.log(sumOfN(1, 2, 3)) // output: 6
+        console.log(sumOfN(1, 2, 3, 4)) // output: 10
 
 
-const sumOfN = (...arr) => {
-    let sum = 0;
-    for (let element of arr) {
-        sum += element
-    }
-     return sum 
-}
+        const sumOfN = (...arr) => {
+            let sum = 0;
+            for (let element of arr) {
+                sum += element
+            }
+            return sum
+        }
 
 
 
 
 
-// write a function which will take an array of "n" numbers as an argument and will return the object containing maximum number, minimum number and avg of all numbers in array.
+        // write a function which will take an array of "n" numbers as an argument and will return the object containing maximum number, minimum number and avg of all numbers in array.
 
- sample e.g. -
- console.log(getOutput([1,2,3,4,5])) // output : {max: 5, min: 1, avg: 3}
+        sample e.g. -
+        console.log(getOutput([1, 2, 3, 4, 5])) // output : {max: 5, min: 1, avg: 3}
 
-note: do it without using any inbuilt function
+        note: do it without using any inbuilt
+        function
 
-const getOutput = arr =>{
-    let maxValue = arr[1];
-    let minValue = arr[1];
-    let sum = 0
-
-
-    arr.map(num => {
-
-    sum+=num
-    if (num > maxValue){
-        maxValue = num
-    }
-
-    if (num < minValue) {
-        minValue = num
-    }
-})
-let average = sum/arr.length
-return {maxValue, minValue, average}
-} 
+        const getOutput = arr => {
+            let maxValue = arr[1];
+            let minValue = arr[1];
+            let sum = 0
 
 
+            arr.map(num => {
 
+                sum += num
+                if (num > maxValue) {
+                    maxValue = num
+                }
 
+                if (num < minValue) {
+                    minValue = num
+                }
+            })
+            let average = sum / arr.length
+            return {
+                maxValue,
+                minValue,
+                average
+            }
+        }
