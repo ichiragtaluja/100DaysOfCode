cc23d07a51b70adfa167d5da2fc4e2a237002010
ichiragtaluja <Ichiragtaluja@gmail.com>
2023-03-21 01:16:52 +0530

    add practice questions

--- Day5/es6+ neog pset4/script.js
+++ Day5/es6+ neog pset4/script.js
@@ -1,90 +1,77 @@
 // Write an ES6 function that accepts an array of integers and returns the maximum element in the array. Avoid using in-built methods.
 
 const getMaxElement = (array) => {
-    let maximumNumber = 0;
-    for( let i = 0; i <= array.length; i ++){
-        maximumNumber = array[i] > maximumNumber ? array[i] : maximumNumber
-    }
-    return maximumNumber
-}
+  let maximumNumber = 0;
+  for (let i = 0; i <= array.length; i++) {
+    maximumNumber = array[i] > maximumNumber ? array[i] : maximumNumber;
+  }
+  return maximumNumber;
+};
 
- let array = [4,78,8,3,6,0,12,34]
-console.log(getMaxElement(array)) // 78
-
-
-
+let array = [4, 78, 8, 3, 6, 0, 12, 34];
+console.log(getMaxElement(array)); // 78
 
 // Write an ES6 function that takes an array of numbers and returns the average of all the numbers. Avoid using in-built methods.
 
 const calculateAverage = (array) => {
-    let sum = 0;
-    for (let i = 0; i < array.length; i ++){
-        sum += array[i]
-    }
-    const average = sum / array.length
-    return average.toFixed(2)
-}
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  const average = sum / array.length;
+  return average.toFixed(2);
+};
 
 console.log(calculateAverage([1, 2, 3, 4, 3, 6])); // 3
 
-
-
 // write an ES6 function that takes an array of numbers and converts even numbers to odd numbers by adding 1 to that number.
 
-const convertEvenToOdd = arr => {
-    let newArr = [];
-    for (let i = 0; i < arr.length; i++){
-         arr[i] % 2 === 0 ? newArr.push(arr[i] + 1) : newArr.push(arr[i])
-    }
-    return newArr
-}
+const convertEvenToOdd = (arr) => {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] % 2 === 0 ? newArr.push(arr[i] + 1) : newArr.push(arr[i]);
+  }
+  return newArr;
+};
 
 var numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(convertEvenToOdd(numArr));
 // [1, 3, 3, 5, 5, 7, 7, 9, 9]
 
-
-
-
-
 // write an ES6 function that takes an array of words and returns an array with all its elements whose length is greater than 5.
-const filterWords = words => {
-    let newArray = [];
-    for (let i = 0 ; i < words.length; i ++){
-        words[i].length > 5 ? newArray.push(words[i]) : newArray
-    }
-    return newArray
-}
+const filterWords = (words) => {
+  let newArray = [];
+  for (let i = 0; i < words.length; i++) {
+    words[i].length > 5 ? newArray.push(words[i]) : newArray;
+  }
+  return newArray;
+};
 
 var words = ["eat", "sleep", "code", "repeat", "neog", "community"];
-console.log(filterWords(words)) // ["repeat", "community"]
-
-
+console.log(filterWords(words)); // ["repeat", "community"]
 
 // 2. Write an ES6 function that takes an array of strings and returns a new array with each string capitalized.
 
-const capitalizeWords = array => {
-    let newArray = []
-    for ( let i = 0; i < array.length; i++) {
-        newArray.push(array[i].toUpperCase())
-    }
-    return newArray
-}
+const capitalizeWords = (array) => {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(array[i].toUpperCase());
+  }
+  return newArray;
+};
 
-console.log(capitalizeWords(["eat", "sleep", "code", "repeat"]))
+console.log(capitalizeWords(["eat", "sleep", "code", "repeat"]));
 // ["EAT", "SLEEP", "CODE", "REPEAT"]
-
-
 
 // Write an ES6 function that takes an array of objects and a property name and returns a new array with only the values of that property. Avoid using in-built methods.
 
 const getValues = (array, propName) => {
-    const newArray = []
-    for (let i = 0; i < array.length; i ++){
-        newArray.push(array[i][propName])
-    }
-    return newArray
-}
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(array[i][propName]);
+  }
+  return newArray;
+};
 
 console.log(
   getValues(
@@ -97,102 +84,94 @@ console.log(
   )
 ); // ["John", "Mary", "Peter"]
 
-
-
 // Write an ES6 function that takes the users' details and returns the data with team ID. Avoid using in-built methods.
 
 const podAndTeamAllocation = (obj) => {
-    obj.teamId = 667543
-    return obj}
+  obj.teamId = 667543;
+  return obj;
+};
 
-const userData = { firstName : "John", lastName: "Dee" }
-console.log(podAndTeamAllocation(userData))
+const userData = { firstName: "John", lastName: "Dee" };
+console.log(podAndTeamAllocation(userData));
 // {firstName: 'John', lastName: 'Dee', teamId: 667543}
-
-
-
 
 // Write an ES6 function which checks if a student already has a team. If team is not given then add them to team “A” and return the object else do nothing and return the same object. Avoid using in-built methods
 //m1
-const checkForTeam = obj => {
-    obj.hasOwnProperty('team') ? obj : obj.team = "A"
-    return obj
-    
-}
+const checkForTeam = (obj) => {
+  obj.hasOwnProperty("team") ? obj : (obj.team = "A");
+  return obj;
+};
 
 //m2
-const checkForTeam = obj => {
-    for (let key in obj){
-        if (key === "team"){
-            return obj
-        }
+const checkForTeam = (obj) => {
+  for (let key in obj) {
+    if (key === "team") {
+      return obj;
     }
-    obj.team = "A"
-    return obj
-}
+  }
+  obj.team = "A";
+  return obj;
+};
 
-console.log(checkForTeam({firstName: 'Penn', lastName: 'Ma'}))
+console.log(checkForTeam({ firstName: "Penn", lastName: "Ma" }));
 // {firstName: 'Penn', lastName: 'Ma', team: A}
 
-console.log(checkForTeam({firstName: 'John', lastName: 'Dee', team: 'B'}))
+console.log(checkForTeam({ firstName: "John", lastName: "Dee", team: "B" }));
 // {firstName: 'John', lastName: 'Dee', team: B}
 
-console.log(checkForTeam({firstName: 'Priya', lastName: 'Raj'}))
+console.log(checkForTeam({ firstName: "Priya", lastName: "Raj" }));
 // {firstName: 'Priya', lastName: 'Raj', team: A}
-
-
-
-
 
 // Destructure the following code to get the desired outputs. Avoid using in-built methods.
 
-const book = { 
-    title: 'JavaScript: The Definitive Guide',  
-    authors: [{name: 'David Flanagan', age: 49 }, { name: 'Yukihiro Matsumoto', age: 57 }],  
-    publisher: {name: 'O\'Reilly Media', location: 'CA'}
+const book = {
+  title: "JavaScript: The Definitive Guide",
+  authors: [
+    { name: "David Flanagan", age: 49 },
+    { name: "Yukihiro Matsumoto", age: 57 },
+  ],
+  publisher: { name: "O'Reilly Media", location: "CA" },
 };
 
 // Your ES6 code here
-const {title, authors:[{name: author1}, {name: author2}], publisher: {name: publisherName}} = book
-
-
+const {
+  title,
+  authors: [{ name: author1 }, { name: author2 }],
+  publisher: { name: publisherName },
+} = book;
 
 console.log(title); // JavaScript: The Definitive Guide
-console.log(author1); // David Flanagan 
+console.log(author1); // David Flanagan
 console.log(author2); // Yukihiro Matsumoto
 console.log(publisherName); // O'Reilly Media
 
-
-
 // Write an ES6 function that takes an array of objects and returns the sum of all ages.
 
-const sumOfAges = arr => {
-    let sumOfAge = 0
-    for (let i = 0; i < arr.length; i++){
-        sumOfAge += arr[i].age
-    }
-    return sumOfAge
-}
+const sumOfAges = (arr) => {
+  let sumOfAge = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sumOfAge += arr[i].age;
+  }
+  return sumOfAge;
+};
 
 var array = [
-	{
-		name: "Jay",
-		age: 60
-	},
-	{
-		name: "Gloria",
-		age: 36
-	},
-	{
-		name: "Manny",
-		age: 16
-	},
-	{
-		name: "Joe",
-		age: 9
-	}
+  {
+    name: "Jay",
+    age: 60,
+  },
+  {
+    name: "Gloria",
+    age: 36,
+  },
+  {
+    name: "Manny",
+    age: 16,
+  },
+  {
+    name: "Joe",
+    age: 9,
+  },
 ];
 
 console.log(sumOfAges(array)); // 121
-
-
--- Day9/script.js
+++ Day9/script.js
@@ -1,30 +1,25 @@
 // Write a function that takes an array and returns all the numbers in a new array increased by 10.
 
-const arrayOfNumbers = [1, 2, 3, 4, 5]
+const arrayOfNumbers = [1, 2, 3, 4, 5];
 
-const increaseNumberBy10 = num => num + 10
+const increaseNumberBy10 = (num) => num + 10;
 
-console.log(arrayOfNumbers.map(increaseNumberBy10))
-
-
+console.log(arrayOfNumbers.map(increaseNumberBy10));
 
 // Write a function that takes an array and returns a new array having even numbers multiplied by 20 and odd numbers multiplied by 30.
 
-const arrayOfNumbers = [1, 2, 3, 4, 5]
+const arrayOfNumbers = [1, 2, 3, 4, 5];
 
-const manupulateArray = num => num % 2 === 0 ? num20 : num30
+const manupulateArray = (num) => (num % 2 === 0 ? num20 : num30);
 
-console.log(arrayOfNumbers.map(manupulateArray))
-
-
+console.log(arrayOfNumbers.map(manupulateArray));
 
 // Write a function that takes an array and returns a new array having even indexes numbers subtracted by 2 and odd indexes numbers added by 3.
 
-const arrayOfNumbers = [1, 2, 3, 4, 5]
+const arrayOfNumbers = [1, 2, 3, 4, 5];
 
-const manupulateArray = (num, index) => index % 2 === 0 ? num - 2 : num + 3
-console.log(arrayOfNumbers.map(manupulateArray))
-
+const manupulateArray = (num, index) => (index % 2 === 0 ? num - 2 : num + 3);
+console.log(arrayOfNumbers.map(manupulateArray));
 
 // const obj = {
 //     a: 10,
@@ -33,28 +28,21 @@ console.log(arrayOfNumbers.map(manupulateArray))
 // }
 // Write a program using destructing to find a number in this object which is divisible both by 3 and 5
 
-const checkFunction = ({
-    a,
-    b,
-    arr
-}) => {
+const checkFunction = ({ a, b, arr }) => {
+  let newArr = [];
 
-    let newArr = []
+  a % 15 === 0 ? newArr.push(a) : null;
+  b % 15 === 0 ? newArr.push(b) : null;
 
-    a % 15 === 0 ? newArr.push(a) : null
-    b % 15 === 0 ? newArr.push(b) : null
+  const pushIfDivisibleBy15 = (num) =>
+    num % 15 === 0 ? newArr.push(num) : null;
 
-    const pushIfDivisibleBy15 = num => num % 15 === 0 ? newArr.push(num) : null
+  arr.map(pushIfDivisibleBy15);
 
-    arr.map(pushIfDivisibleBy15)
+  return newArr;
+};
 
-
-    return newArr
-}
-
-console.log(checkFunction(obj))
-
-
+console.log(checkFunction(obj));
 
 // const obj = {
 //     details: [{name: 'Ankit', company: 'Chalo'},
@@ -65,31 +53,26 @@ console.log(checkFunction(obj))
 // // Output: ['Chalo', 'Razorpay', 'No Broker']
 
 const obj = {
-    details: [{
-            name: 'Ankit',
-            company: 'Chalo'
-        },
-        {
-            name: 'Rohan',
-            company: 'Razorpay'
-        },
-        {
-            name: 'Shivam',
-            company: 'No Broker'
-        }
-    ]
-}
+  details: [
+    {
+      name: "Ankit",
+      company: "Chalo",
+    },
+    {
+      name: "Rohan",
+      company: "Razorpay",
+    },
+    {
+      name: "Shivam",
+      company: "No Broker",
+    },
+  ],
+};
 
-const extractCompny = ({
-    company
-}) => company
-const anonymousFunction = ({
-    details
-}) => details.map(extractCompny)
+const extractCompny = ({ company }) => company;
+const anonymousFunction = ({ details }) => details.map(extractCompny);
 
-console.log(anonymousFunction(obj))
-
-
+console.log(anonymousFunction(obj));
 
 // const obj = {
 //     details: [{name: 'Ankit', company: 'Chalo'},
@@ -100,32 +83,27 @@ console.log(anonymousFunction(obj))
 // Write a function that takes the above object and returns a new array containing the company names for people having names starting with A.
 
 const obj = {
-    details: [{
-            name: 'Ankit',
-            company: 'Chalo'
-        },
-        {
-            name: 'Rohan',
-            company: 'Razorpay'
-        },
-        {
-            name: 'Shivam',
-            company: 'No Broker'
-        }
-    ]
-}
+  details: [
+    {
+      name: "Ankit",
+      company: "Chalo",
+    },
+    {
+      name: "Rohan",
+      company: "Razorpay",
+    },
+    {
+      name: "Shivam",
+      company: "No Broker",
+    },
+  ],
+};
 
-const extractCompny = ({
-    name,
-    company
-}) => name[0] === "A"
+const extractCompny = ({ name, company }) => name[0] === "A";
 
+const findCompany = (obj) => obj.company;
 
-const findCompany = obj => obj.company
+const anonymousFunction = ({ details }) =>
+  details.filter(extractCompny).map(findCompany);
 
-
-const anonymousFunction = ({
-    details
-}) => details.filter(extractCompny).map(findCompany)
-
-console.log(anonymousFunction(obj))
+console.log(anonymousFunction(obj));
