271471d13c5afb5ebdae3c3c632196d349204a09
ichiragtaluja <Ichiragtaluja@gmail.com>
2023-05-06 01:14:29 +0530

    add 2 leetcode questions

--- Day55/q1.js
+++ Day55/q1.js
@@ -10,3 +10,24 @@ var canConstruct = function(ransomNote, magazine) {
     return (!ransomNote ? true : false)     
     
 };
+
+
+
+// Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements in the array, it should return -1.
+
+
+Array.prototype.last = function() {
+    return (this.length ? this[this.length-1] : -1)
+}
+
+
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+var createCounter = function(n) {
+   
+    let count = n 
+    return function(){
+        count++;
+        return count - 1
+    }
+ };
