c0628d1351064dae84e518f19aa9611dff9a99d5
ichiragtaluja <Ichiragtaluja@gmail.com>
2023-05-07 00:28:14 +0530

    add leetcode questions

--- Day56/index.js
+++ Day56/index.js
@@ -23,3 +23,33 @@ var plusOne = function(digits) {
     return r.split("")
     
 };
+
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+var twoSum = function(nums, target) {
+    let output = []
+    for (let i = 0; i < nums.length; i++){
+        for (let j = 0; j < i; j++){
+            if (nums[i] + nums[j] === target){
+                output.push(j);
+                output.push(i);
+                return output
+            }
+        }
+    }
+};
+
+
+
+// Given an integer x, return true if x is a 
+// palindrome
+// , and false otherwise.
+
+var isPalindrome = function(x) {
+    return x === Number(x.toString().split("").reverse().join("")) 
+};
